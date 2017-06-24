564f6f57198f86ac5d1dfb23293e19848e01dda7
LiaoJunCong <liaojuncong@foxmail.com>
2017-06-25 00:33:59 +0800

    MyBatis 插件及示例 — 打印每条 SQL 语句及其执行时间

--- web/src/main/java/org/cong/x/project/web/conf/MyBatisConfig.java
+++ web/src/main/java/org/cong/x/project/web/conf/MyBatisConfig.java
@@ -3,6 +3,7 @@ package org.cong.x.project.web.conf;
 import com.github.pagehelper.PageHelper;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.cong.x.project.web.interceptor.SqlCostInterceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
@@ -34,7 +35,7 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
     public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
-        sqlSessionFactoryBean.setTypeAliasesPackage("com.footballschool.model");
+        sqlSessionFactoryBean.setTypeAliasesPackage("org.cong.x.project.model");
 
         //分页插件
         PageHelper pageHelper = new PageHelper();
@@ -44,7 +45,7 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
         properties.setProperty("returnPageInfo", "check");
         properties.setProperty("params", "count=countSql");
         pageHelper.setProperties(properties);
-        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageHelper});
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageHelper, new SqlCostInterceptor()});
 
         //XML目录
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
